b9e8185dcf479e93e31dad3211c12932481df82e
Shsonu33 <189646112+Shsonu33@users.noreply.github.com>
2025-01-09 22:30:02 +0530

    Add files via upload

---
+++ hunter.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7514830895:AAE1nsNMyJJqBoPJ9oTD_7_nI7KdKQJIovE")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Itz_sonu_9")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("859818729")
+
+
+
+
+
+  
